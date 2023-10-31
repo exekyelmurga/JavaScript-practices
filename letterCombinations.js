b0fe9af5fa032927b704b75268efd230bef3b36c
exekyelmurga <98778181+exekyelmurga@users.noreply.github.com>
2023-10-30 23:44:38 -0300

    Update letterCombinations.js

--- letterCombinations.js
+++ letterCombinations.js
@@ -10,27 +10,27 @@ const phoneKeypadMapping = {
 };
 
 function letterCombinations(digits, output = '') {
-  // If the array of digits is empty, return the output string.
+  // Validate the input.
   if (digits.length === 0 || digits.length > 4 || digits.includes(0) || digits.includes(1)) {
       return [output];
   }
-  // Get the first number from the input received as a parameter.
+  // Get the first digit.
   const firstDigit = digits[0];
 
-  // Get the letters corresponding to the first digit from the input string received as a parameter.
+  // Get the letters associated with the first digit.
   const letters = phoneKeypadMapping[firstDigit];
 
-  // Create a new array of digits without the first digit. In each recursive iteration, the letters decrease until the end of the loop.
+   // Get the remaining digits.
   const remainingDigits = digits.slice(1);
   
-  // Create an empty array to store the remaining letter combinations. In each iteration, the array is filled with different combinations.
+  // Create an empty list to store the remaining combinations.
   const remainingCombinations = [];
 
-  // Iterate over the letters of the first digit passed as a parameter.
+  // Iterate over the letters associated with the first digit.
   for (const letter of letters) {
-      // Call the function recursively with the remaining letters and the current output string plus the current letter.
-      // When passing remainingDigits as an empty string at the end of the iteration, it returns output as in the first 'if' statement,
-      // and that's when it is stored in 'remainingDigits.'
+      // Recursively call the function with the remaining digits and the current output string plus the current letter.
+      // When passing an empty string as the remaining digits, the function returns the output string, just like the first if statement.
+      // This is how the output strings are stored in remainingCombinations.
       const newCombinations = letterCombinations(remainingDigits, output + letter);
 
       // Add the new combinations to the array of remaining combinations.
